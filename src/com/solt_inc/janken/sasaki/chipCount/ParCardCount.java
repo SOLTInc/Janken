c6cc222e3fe2a674744bcdb496db378140ecfbad
naoki.sasaki <n.sasaki@solt-inc.com>
2017-07-17 01:46:48 +0900

    佐々木_カイジ限定ジャンケン作成

---
+++ src/com/solt_inc/janken/sasaki/chipCount/ParCardCount.java
@@ -0,0 +1,9 @@
+package com.solt_inc.janken.sasaki.chipCount;
+
+public class ParCardCount {
+
+	public static int countSub(int cardCount) {
+		int parCardCount = cardCount - 1;
+		return parCardCount;
+	}
+}
